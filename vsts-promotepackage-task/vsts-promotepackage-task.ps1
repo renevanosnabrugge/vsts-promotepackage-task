187bb955320d519807921e8491fb4ae74857d0d0
ONVZ\RvOsnabrugge <rvosnabrugge@onvz.nl>
2017-05-18 16:24:07 +0200

    Changed Package feedtype and retrieval of Package

--- vsts-promotepackage-task/vsts-promotepackage-task.ps1
+++ vsts-promotepackage-task/vsts-promotepackage-task.ps1
@@ -5,12 +5,10 @@ $feedName = Get-VstsInput -Name feed
 $packageId = Get-VstsInput -Name definition
 $packageVersion =Get-VstsInput -Name version
 $releaseView =Get-VstsInput -Name releaseView
-$feedType =Get-VstsInput -Name feedType
 
-$baseurl = $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI 
-$baseprojecturl += $baseurl +  $env:SYSTEM_TEAMPROJECT + "/_apis"
-$basefeedsurl = $baseurl -replace ".visualstudio", ".feeds.visualstudio"
-
+$account = ($env:SYSTEM_TEAMFOUNDATIONSERVERURI -replace "https://(.*)\.visualstudio\.com/", '$1').split('.')[0]
+$basepackageurl = ("https://{0}.pkgs.visualstudio.com/DefaultCollection/_apis/packaging/feeds" -f $account)
+$basefeedsurl = ("https://{0}.feeds.visualstudio.com/DefaultCollection/_apis/packaging/feeds" -f $account)
 
 function InitializeRestHeaders()
 {
@@ -51,13 +49,20 @@ function ValidatePatToken($token)
 
 function Set-PackageQuality
 {
-   
+   # First get the name of the package (strange REST API behavior) and the Package Feed Type
+   $packageURL = "$basefeedsurl/$feedName/packages/$packageId/?api-version=2.0-preview"
+   $packResponse = Invoke-RestMethod -Uri $packageURL -Headers $headers -ContentType "application/json" -Method Get 
+
+   $feedType = $packResponse.protocolType
+   $packageName = $packResponse.normalizedName
+
+
     #API URL is slightly different for npm vs. nuget...
     switch($feedType)
     {
-        "npm" { $releaseViewURL = "$basepackageurl/$feedName/$feedType/$packageId/versions/$($packageVersion)?api-version=3.0-preview.1" }
-        "nuget" { $releaseViewURL = "$basepackageurl/$feedName/$feedType/packages/$packageId/versions/$($packageVersion)?api-version=3.0-preview.1" }
-        default { $releaseViewURL = "$basepackageurl/$feedName/$feedType/packages/$packageId/versions/$($packageVersion)?api-version=3.0-preview.1" }
+        "npm" { $releaseViewURL = "$basepackageurl/$feedName/$feedType/$packageName/versions/$($packageVersion)?api-version=3.0-preview.1" }
+        "nuget" { $releaseViewURL = "$basepackageurl/$feedName/$feedType/packages/$packageName/versions/$($packageVersion)?api-version=3.0-preview.1" }
+        default { $releaseViewURL = "$basepackageurl/$feedName/$feedType/packages/$packageName/versions/$($packageVersion)?api-version=3.0-preview.1" }
     }
     
      $json = @{
@@ -67,7 +72,7 @@ function Set-PackageQuality
             value = "$releaseView"
         }
     }
-
+Write-Host $releaseViewURL
     $response = Invoke-RestMethod -Uri $releaseViewURL -Headers $headers   -ContentType "application/json" -Method Patch -Body (ConvertTo-Json $json)
     return $response
 }
