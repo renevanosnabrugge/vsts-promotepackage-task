c3d3c7abac4660054645830e917433ebaccce4fc
Alexander Merz <zoamer@zeiss.com>
2020-03-10 14:39:55 +0100

    Fix $IsWindows is read-only since PS 6.0 #49

--- vsts-promotepackage-task/vsts-promotepackage-task.ps1
+++ vsts-promotepackage-task/vsts-promotepackage-task.ps1
@@ -235,12 +235,9 @@ function Get-NuGetPackageMetadata([string]$filePath) {
 
 function Get-NpmPackageMetadata([string]$filePath) {
     $package = New-PackageObject
-    $tempDir = New-TemporaryDirectory
+    $tempDir = New-TemporaryDirectory    
     try {
-        if ($IsWindows -eq $null) {
-            $IsWindows = $Env:OS.StartsWith('Windows')
-        }
-        $flags = if ($IsWindows) { '--force-local -xvzf' } else { 'xvzf' }
+        $flags = if ($IsWindows -or $Env:OS.StartsWith('Windows')) { '--force-local -xvzf' } else { 'xvzf' }
         tar $flags `"$filePath`" -C `"$tempDir`" 2> $null
         $packageJsonPath = "$tempDir/package/package.json"
         $packageJson = Get-Content -Raw -Path $packageJsonPath | ConvertFrom-Json
